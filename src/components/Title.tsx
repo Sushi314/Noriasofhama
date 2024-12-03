791e6a95f77d73e14dba448a1dc0c32e919e9a06
Sushi <98758109+Sushi314@users.noreply.github.com>
2024-12-03 10:34:46 -0500

    added background image

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,23 @@
+
 const Title = () => {
     return (
       <section id="title" className="title">
         <h1>Analysis of the Ancient Waterworks of the Orontes</h1>
         <p>The evolution of hydraulic technology in ancient Syria, and the Norias of Hama's contributions to sustainable water management.
+        </p>
+        <p
+          style={{
+            backgroundImage: `url("/assets/images/Pano Archnet.jpg")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "400px",
+            borderRadius: ".15em",
+            maskImage: "radial-gradient(circle, rgba(0,0,0,1) 90%, rgba(0,0,0,0) 100%)",
+
+
+          }}>
         </p>
       </section>
     );
